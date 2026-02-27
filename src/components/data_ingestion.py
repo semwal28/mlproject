1548df469f2fa667cc1c7934c2427cf77631a898
Dhruv Semwal <dhruvsemwal51@gmail.com>
2026-02-27 16:51:43 +0530

    Model Trainer

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -12,6 +12,9 @@ from src.logger import logging
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
 
+from src.components.model_trainer import ModelTrainer
+from src.components.model_trainer import ModelTrainerConfig
+
 
 @dataclass
 class DataIngestionConfig:
@@ -72,5 +75,11 @@ class DataIngestion:
 if __name__ == "__main__":
     obj = DataIngestion()
     train_data, test_data = obj.initiate_data_ingestion()
+
     data_transformation = DataTransformation()
-    data_transformation.initiate_data_transformation(train_data, test_data)
+    train_arr, test_arr, _ = data_transformation.initiate_data_transformation(train_data, test_data)
+
+    modeltrainer = ModelTrainer()
+    r2 = modeltrainer.initiate_model_trainer(train_arr, test_arr)
+
+    print(f"Final R2 Score: {r2}")
